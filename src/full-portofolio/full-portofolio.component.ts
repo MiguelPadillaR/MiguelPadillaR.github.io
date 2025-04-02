be5360b089f020e916cca47c2c505906bf00091a
Miguel Padilla <0619889697@uma.es>
2025-04-02 20:10:26 +0200

    Updated and styled Stacks section

--- src/full-portofolio/full-portofolio.component.ts
+++ src/full-portofolio/full-portofolio.component.ts
@@ -10,13 +10,14 @@ import { Component } from '@angular/core';
 export class FullPortofolioComponent {
 
   public stacks = [
-    { name: 'Angular', icon: 'assets/icons/angular.svg' },
-    { name: 'TypeScript', icon: 'assets/icons/typescript.svg' },
-    { name: 'HTML5', icon: 'assets/icons/html5.svg' },
-    { name: 'CSS3', icon: 'assets/icons/css3.svg' },
-    { name: 'Git', icon: 'assets/icons/git.svg' },
-    { name: 'Linux', icon: 'assets/icons/linux.svg' },
-    { name: 'Python', icon: 'assets/icons/python.svg' }
+    { name: 'Angular', icon: 'assets/icons/angular.svg', link: 'https://angular.dev' },
+    { name: 'TypeScript', icon: 'assets/icons/typescript.svg', link: 'https://www.typescriptlang.org' },
+    { name: 'HTML5', icon: 'assets/icons/html5.png', link: 'https://www.w3schools.com/html/' },
+    { name: 'CSS3', icon: 'assets/icons/css3.png', link: 'https://www.w3schools.com/css/' },
+    { name: 'Git', icon: 'assets/icons/git.svg', link: 'https://github.com' },
+    { name: 'Linux', icon: 'assets/icons/linux.svg', link: 'https://www.linux.org' },
+    { name: 'Python', icon: 'assets/icons/python.svg', link: 'https://www.python.org' },
+    { name: 'Java', icon: 'assets/icons/java.svg', link: 'https://www.java.com/es/' }
   ];
 
 }
