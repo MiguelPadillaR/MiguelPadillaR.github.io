0b118430503e2a7bc2b43075897a7eb40eba0e33
Miguel Padilla <0619889697@uma.es>
2025-04-03 17:42:59 +0200

    Updated and styled Project section

--- src/full-portofolio/full-portofolio.component.ts
+++ src/full-portofolio/full-portofolio.component.ts
@@ -20,4 +20,31 @@ export class FullPortofolioComponent {
     { name: 'Java', icon: 'assets/icons/java.svg', link: 'https://www.java.com/es/' }
   ];
 
+  public personalProjects = [
+    { 
+      name: 'PINETool',
+      img:'assets/img/PINETool-logo.png',
+      desc: "The Picasso Integrated-Network Expression Tool is the author's final degree project commissioned by the Molecular Biology and Biotechnology (BIO-114) research group) research group at Universidad de Málaga. It serves as a tool to explore customized genomic databases with ease to retieve data. It uses the NCBI CLI and the most valuable resource of the Super Computation and Bio-Innovation center: Picasso.",
+      link: ''
+    },
+    { 
+      name: 'C-PRX',
+      img:'assets/img/CPRX-logo.png',
+      desc: "Started during and internship and unfinished because of time constraints, the Proximity Center tool (C-PRX from its initials in Spanish) was design to relieve medical personnel's workload by automatically assigning a patient to the nearest, most suitable and available healthcare center for their medical imaging needs.",
+      link: ''
+    },
+    { 
+      name: 'Misc#1',
+      img:'',
+      desc: 'Some desc',
+      link: ''
+    },
+    { 
+      name: 'Misc#2',
+      img:'',
+      desc: 'Some desc',
+      link: ''
+    },
+  ]
+
 }
